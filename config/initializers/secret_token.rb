95ecb3ec0b14b890975134d23c495bf31d945d02
Mike Williamson <blessedbyvirtuosity@gmail.com>
2011-01-17 12:18:03 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Angola::Application.config.secret_token = '6ecd79f270585e7e3b73c2b028d53b2a950ca82724fe86d6cca7a3c58f1ac25cca4887c2dda729ef8fc13979effcfbb9f8650e34fecbead2c3b4c9e28168ee62'
