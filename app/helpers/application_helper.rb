53c195dab8355b5fdb78bfb1f39b9b60577c3c3c
Mike Williamson <blessedbyvirtuosity@gmail.com>
2011-04-06 22:06:59 -0400

    Added language switching.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def language_switcher
+    content_tag :span, :id => "language_switcher" do
+      links = ""
+      links += link_to image_tag('gb.gif'), url_for(:locale => 'en') unless (I18n.locale.to_s == 'en')
+      links += link_to image_tag('br.gif'), url_for(:locale => 'br') unless (I18n.locale.to_s == 'br')
+      links.html_safe
+    end
+  end
 end
