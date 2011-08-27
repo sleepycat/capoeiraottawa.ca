1be978450b8d214854c298d3ffce62b6d7b4ccba
Mike Williamson <blessedbyvirtuosity@gmail.com>
2011-08-26 22:26:09 -0400

    Added French translation.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,6 +16,7 @@ module ApplicationHelper
   def language_switcher
     content_tag :span, :id => "language_switcher" do
       links = ""
+      links += link_to image_tag('fr.png'), url_for(:locale => 'fr') unless (I18n.locale.to_s == 'fr')
       links += link_to image_tag('gb.gif'), url_for(:locale => 'en') unless (I18n.locale.to_s == 'en')
       links += link_to image_tag('br.gif'), url_for(:locale => 'br') unless (I18n.locale.to_s == 'br')
       links.html_safe
