07b646976ce7dc82b3a323e148b89190884e3cfb
Mike Williamson <blessedbyvirtuosity@gmail.com>
2011-09-11 20:30:03 -0400

    Made description field optional on video model.

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,4 +1,4 @@
 class Video < ActiveRecord::Base
   translates :title, :description
-  validates :youtube_id, :title, :duration, :description, :uploader, :large_thumbnail, :small_thumbnail, :url, :presence => true
+  validates :youtube_id, :title, :duration, :uploader, :large_thumbnail, :small_thumbnail, :url, :presence => true
 end
