24c4234b52e970b67bfc333f1ea880908d8056c2
Mike Williamson <blessedbyvirtuosity@gmail.com>
2012-09-11 23:02:34 -0400

    Now able to delete pages

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,12 @@ Angola::Application.routes.draw do
     get "login", to: "sessions#index", as: :login
     delete "logout", to: "sessions#destroy", as: :logout
     resources :videos, :practices, :locations
-    resources :pages, except: [:show, :edit, :update, :new]
     get "pages/new", to: "pages#new", as: :new_page
+    post "pages", to: "pages#create", as: :pages
     put ":page_title", to: "pages#update"
-    get "pages/:page/edit", to: "pages#edit", as: :edit_page
+    get ":page/edit", to: "pages#edit", as: :edit_page
     get ":page", to: "pages#show", as: :page
+    delete ":page", to: "pages#destroy", as: :page
     get "", to: "pages#index"
 
   end
