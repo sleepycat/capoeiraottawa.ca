3c2b0a3cfb49ee074a9f7494f16a1f985412c324
Mike Williamson <blessedbyvirtuosity@gmail.com>
2012-09-13 17:43:11 -0400

    Added validation to page model

--- app/models/page.rb
+++ app/models/page.rb
@@ -3,6 +3,7 @@ class Page < ActiveRecord::Base
   translates :title, :content
   attr_accessible :content, :sort_order, :title
   validates_uniqueness_of :title
+  validates_presence_of :title, :content
 
   def to_param
     self.title
