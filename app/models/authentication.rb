4220c7a72a71352f61b6014d1db26e14ee462d17
Mike Williamson <blessedbyvirtuosity@gmail.com>
2012-09-15 12:48:13 -0400

    Validate that uid and provider are unique

--- app/models/authentication.rb
+++ app/models/authentication.rb
@@ -1,6 +1,8 @@
 class Authentication < ActiveRecord::Base
   belongs_to :user
   attr_accessible :create, :destroy, :index, :provider, :uid, :user_id
+  #ensure that the combination of uid and provider is unique
+  validates_uniqueness_of :uid, scope: :provider
 
 
   def self.from_omniauth(auth)
