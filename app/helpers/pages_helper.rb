c42d6060ff7bc8a825aa01fa7357b7828d0a4ef3
Mike Williamson <blessedbyvirtuosity@gmail.com>
2012-09-20 17:25:11 -0400

    Fixed missing translations

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,6 +1,6 @@
 module PagesHelper
   def action_links
     #TODO - get the confirm option working here.
-    "#{ link_to t('edit'), edit_page_path(@page), class: 'action_link' } #{ button_to(t('delete'), {controller: "pages", action: "destroy", page: @page.title}, {method: :delete, confirm: "Are you sure?", class: "destroy_button action_link"})} #{link_to t('page.new'), new_page_path, class: 'action_link'}".html_safe
+    "#{ link_to t('edit'), edit_page_path(@page), class: 'action_link' } #{ button_to(t('destroy'), {controller: "pages", action: "destroy", page: @page.title}, {method: :delete, confirm: "Are you sure?", class: "destroy_button action_link"})} #{link_to t('new'), new_page_path, class: 'action_link'}".html_safe
   end
 end
