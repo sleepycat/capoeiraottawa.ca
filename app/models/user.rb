5d85f4babfe2f089c0d79f7c1f122baa24fbff17
Mike Williamson <blessedbyvirtuosity@gmail.com>
2012-09-21 17:05:52 -0400

    Fixed hash syntax for user model.
    I18n.t syntax is melting my brain.

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,10 +23,11 @@ class User < ActiveRecord::Base
 
   def self.from_omniauth(auth)
     create do |user|
-      user.name = auth["info.name"] if auth["info.name"]
-      user.nickname = auth["info.nickname"] if auth["info.nickname"]
-      user.image = auth["info.image"] if auth["info.image"]
-      user.website = auth["urls.website"] if auth["urls.website"]
+      user.name = auth["info"]["name"] if auth["info"]["name"]
+      user.nickname = auth["info"]["nickname"] if auth["info"]["nickname"]
+      user.image = auth["info"]["image"] if auth["info"]["image"]
+      user.website = auth["urls"]["website"] if auth["urls"]["website"]
+      user.email = auth["info"]["email"] if auth["urls"]["email"]
       user.authentications << Authentication.from_omniauth(auth)
     end
   end
