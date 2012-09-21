423da5ee953f12f068591a68d9548993a3f7c501
Mike Williamson <blessedbyvirtuosity@gmail.com>
2012-09-21 17:09:29 -0400

    removed debugging statement

--- app/models/authentication.rb
+++ app/models/authentication.rb
@@ -7,7 +7,6 @@ class Authentication < ActiveRecord::Base
 
   def self.from_omniauth(auth)
     create do |authentication|
-      puts auth.to_yaml
       authentication.uid = auth["uid"]
       authentication.provider = auth["provider"]
     end
