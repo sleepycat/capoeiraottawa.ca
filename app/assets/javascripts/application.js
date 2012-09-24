0f37f7e4c5960c572ff11904d4965c63201e38a5
Mike Williamson <blessedbyvirtuosity@gmail.com>
2012-09-23 22:34:28 -0400

    Setting background-size based on screen res

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,5 +2,7 @@
 //= require rails
 //= require_self
 $(document).ready(function(){
+  console.log('testing!')
+  $('body').css({'background-size': screen.width + 'px ' + screen.height + 'px' });
   $('.notice').delay(2000).fadeOut(2000);
 });
