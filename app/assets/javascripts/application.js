14d300699938925311ff88d871c2decfc7e79578
Mike Williamson <blessedbyvirtuosity@gmail.com>
2012-09-23 23:04:45 -0400

    Resizing background with js for other controllers

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,8 +1,8 @@
 //= require jquery
 //= require rails
 //= require_self
+$('body').css({'background-size': screen.width + 'px ' + screen.height + 'px' });
+
 $(document).ready(function(){
-  console.log('testing!')
-  $('body').css({'background-size': screen.width + 'px ' + screen.height + 'px' });
   $('.notice').delay(2000).fadeOut(2000);
 });
