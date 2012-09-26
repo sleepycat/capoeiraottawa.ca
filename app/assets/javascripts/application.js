80d6fd4e6031e7229533b4567ce92b898946152a
Mike Williamson <blessedbyvirtuosity@gmail.com>
2012-09-25 20:19:40 -0400

    Rip and replace backgroundSize with anystretch

--- app/assets/javascripts/application.js
+++
@@ -1,9 +0,0 @@
-//= require jquery
-//= require jquery.backgroundSize
-//= require rails
-//y require_self
-//$('body').css({'background-size': screen.width + 'px ' + screen.height + 'px' });
-$('body').css("background-size", "cover")
-$(document).ready(function(){
-  $('.notice').delay(2000).fadeOut(2000);
-});
